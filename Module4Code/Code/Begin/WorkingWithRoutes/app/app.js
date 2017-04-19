95948c349b2a0b8c6b7c084c6de6b5765e425c69
Semih Şentürk <semihsenturk1983@gmail.com>
2017-04-19 13:52:59 +0300

    updated module 4

--- Module4Code/Code/Begin/WorkingWithRoutes/app/app.js
+++ Module4Code/Code/Begin/WorkingWithRoutes/app/app.js
@@ -1,15 +1,18 @@
-(function () {
-
+(function() {
+    
     var app = angular.module('customersApp', ['ngRoute']);
-
-    app.config(function ($routeProvider) {
+    
+    app.config(function($routeProvider) {
         $routeProvider
             .when('/', {
                 controller: 'CustomersController',
                 templateUrl: 'app/views/customers.html'
             })
-            .otherwise({ redirectTo: '/' });
-
-    })
-
+            .when('/orders/:customerId', {
+                controller: 'OrdersController',
+                templateUrl: 'app/views/orders.html'
+            })
+            .otherwise( { redirectTo: '/' } );
+    });
+    
 }());
